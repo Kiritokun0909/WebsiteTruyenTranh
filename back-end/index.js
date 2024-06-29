c81251252ee347233556189fde63a7ab47a2eb3d
Kiritokun0909 <hoduchoang0909@gmail.com>
2024-06-29 15:09:48 +0700

    connect database mysql into nodejs

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,26 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const morgan = require('morgan')
+const bodyParser = require('body-parser');
+const routesHandler = require('./routes/handler.js');
+const db = require('./db.js');
 
-// route 
-app.get('/trang-chu', (req, res) => {
-  res.send('Hello World!')
-})
+data = db.query(
+  'SELECT * FROM role'
+).then(data => console.log(data[0]))
+.catch(err => console.log('Failed connect database\n'))
+
+
+const app = express();
+const port = 4000;
+
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use('/', routesHandler);
 
 // Ip 127.0.0.1 - localhost
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}/`)
-})
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
+
